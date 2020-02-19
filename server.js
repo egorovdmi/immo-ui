1957401880b9afab8ab1b6cbcd737a769e372eb0
Dmitry Egorov <dmitry.egorov@ottonova.de>
2020-02-19 15:54:37 +0100

    added express service hosting

---
+++ server.js
@@ -0,0 +1,23 @@
+var express = require("express");
+var join = require("path").join;
+
+// Express server
+const app = express();
+
+const PORT = process.env.PORT || 80;
+const DIST_FOLDER = join(process.cwd(), "dist");
+
+console.log(process);
+
+// Server static files from /browser
+app.get("*.*", express.static(DIST_FOLDER));
+
+// All regular routes use the Universal engine
+app.get("*", (req, res) => {
+  res.sendFile("index.html", { root: DIST_FOLDER });
+});
+
+// Start up the Node server
+app.listen(PORT, () => {
+  console.log(`Node server listening on port ${PORT}`);
+});
