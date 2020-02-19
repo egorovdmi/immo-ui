c85eadd2addf84dd72e7e275e67a939147d741b5
Dmitry Egorov <dmitry.egorov@ottonova.de>
2020-02-19 15:18:07 +0100

    upgraded packages and firebase setup.

--- src/store/messaging-plugin.js
+++ src/store/messaging-plugin.js
@@ -11,7 +11,13 @@ import {
 } from "./constants";
 
 firebase.initializeApp({
-  messagingSenderId: process.env.VUE_APP_FIREBASE_SERVER_ID
+  messagingSenderId: process.env.VUE_APP_FIREBASE_SERVER_ID,
+  projectId: process.env.VUE_APP_FIREBASE_PROJECT_ID,
+  apiKey: process.env.VUE_APP_FIREBASE_API_KEY,
+  appId: process.env.VUE_APP_FIREBASE_APP_ID,
+  authDomain: process.env.VUE_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.VUE_APP_FIREBASE_DATABASE_URL,
+  storageBucket: process.env.VUE_APP_FIREBASE_STORAGE_BUCKET
 });
 
 const messaging = firebase.messaging();
